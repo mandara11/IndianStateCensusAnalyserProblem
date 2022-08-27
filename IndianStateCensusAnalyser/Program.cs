40b0e033543f11620c6b39375066bde0f75bb800
mandara11 <mandaram2345@gmail.com>
2022-08-27 21:11:31 +0530

    Add project files.

---
+++ IndianStateCensusAnalyser/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IndianStateCensusAnalyser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indian State Census Analyser");
+        }
+    }
+}
